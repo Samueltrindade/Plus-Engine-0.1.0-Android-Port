52b9d0148a0e8f21dafc3088c31bbe3fb962eb89
Samueltrindade <90457866+Samueltrindade@users.noreply.github.com>
2022-03-06 12:21:43 -0300

    first commit

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -15,8 +15,7 @@ class CoolUtil
 	public static var difficultyStuff:Array<Dynamic> = [
 		['Easy', '-easy'],
 		['Normal', ''],
-		['Hard', '-hard']
-		['SUPER', '-super']
+		['Hard', '-hard']	
 	];
 
 	public static function difficultyString():String
