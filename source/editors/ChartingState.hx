cd7c0594e73734dce91679e3219917c0fbd82e43
Samueltrindade <90457866+Samueltrindade@users.noreply.github.com>
2022-03-06 12:27:40 -0300

    first commit

--- source/editors/ChartingState.hx
+++ source/editors/ChartingState.hx
@@ -59,7 +59,6 @@ class ChartingState extends MusicBeatState
 		'Hurt Note',
 		'GF Sing',
 		'No Animation'
-		'Bullet notes'
 	];
 	private var noteTypeIntMap:Map<Int, String> = new Map<Int, String>();
 	private var noteTypeMap:Map<String, Null<Int>> = new Map<String, Null<Int>>();
